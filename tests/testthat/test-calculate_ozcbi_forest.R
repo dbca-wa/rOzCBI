02833fd5b43ddd252140e9df4f132e7fcc7229fc
Florian Mayer <florian.mayer@dpc.wa.gov.au>
2023-08-10 15:33:07 +0800

    Persuade GHA for test coverage to use coveralls
    * coverage seems deactivated by dbca-wa admins

--- tests/testthat/test-calculate_ozcbi_forest.R
+++ tests/testthat/test-calculate_ozcbi_forest.R
@@ -3,7 +3,8 @@ test_that("calculate_ozcbi_forest works with missing data", {
   expect_equal(calculate_ozcbi_forest(stratum_1_surface_p_s1_unburnt = 0.5), 0.5)
   expect_equal(calculate_ozcbi_forest(
     stratum_5_overstorey_p_s5_crown = 2.0,
-    stratum_5_overstorey_p_s5_fcov = 0.5
+    stratum_5_overstorey_p_s5_fcov = 0.5,
+    verbose = TRUE
   ), 2 / 3)
 })
 
@@ -21,6 +22,7 @@ test_that("calculate_ozcbi_forest works with complete data", {
     stratum_3_elevated_p_s3_fcov = 0,
     straum_4_intermediate_p_s4_fcov = 0,
     stratum_5_overstorey_p_s5_fcov = 0,
+    verbose = TRUE
   )
   expect_equal(x, 1)
 })
